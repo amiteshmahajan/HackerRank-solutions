a647459ba771cabc0b19abb6e1bb96a5f28ae9f3
amiteshmahajan <121amitesh@gmail.com>
2018-03-10 16:26:39 -0700

    Commit _01

---
+++ src/Java_Stdin_and_Stdout_I/Solution.java
@@ -0,0 +1,17 @@
+package Java_Stdin_and_Stdout_I;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a);
+        a = scan.nextInt();
+        System.out.println(a);
+        a = scan.nextInt();
+        System.out.println(a);
+        // Complete this line
+        // Complete this line
+    }
+}
---
+++ src/Welcome_to_Java/Welcome_to_Java.java
@@ -0,0 +1,11 @@
+package Welcome_to_Java;
+
+public class Welcome_to_Java {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+
+}
